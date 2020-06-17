1d689df2f4db2c2b5a14e711ae4c45eb8db61fe1
Messias Tayllan <messiastayllan@gmail.com>
2020-06-17 17:22:57 -0300

    Add admins and update schema

--- db/migrate/20200617202044_create_contacts.rb
+++ db/migrate/20200617202044_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
-      t.int :kind
+      t.integer :kind
       t.string :value
       t.references :organization, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_201619) do
+ActiveRecord::Schema.define(version: 2020_06_17_202157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,12 +51,47 @@ ActiveRecord::Schema.define(version: 2020_06_17_201619) do
     t.index ["organization_id"], name: "index_addresses_on_organization_id"
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "kind"
+    t.string "value"
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_contacts_on_organization_id"
+  end
+
   create_table "medics", force: :cascade do |t|
     t.string "name"
     t.bigint "organization_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["organization_id"], name: "index_medics_on_organization_id"
+  end
+
+  create_table "organization_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_organization_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_organization_users_on_organization_id"
+    t.index ["reset_password_token"], name: "index_organization_users_on_reset_password_token", unique: true
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -71,5 +106,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_201619) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "organizations"
+  add_foreign_key "contacts", "organizations"
   add_foreign_key "medics", "organizations"
+  add_foreign_key "organization_users", "organizations"
 end
