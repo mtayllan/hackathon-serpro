a8e4d4fd3d851c30a63d1301c36e4217ee0e3b2f
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2020-06-21 16:29:57 -0300

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,6 +35,7 @@ Organization.create(
         city: 'Itapipoca',
         state: 'Ceará',
         street: 'Rua Caio Prado',
+        neighborhood: 'Nova Aldeota',
         number: '1045'
       }
     },
@@ -50,7 +51,8 @@ Organization.create(
       address_attributes: {
         city: 'Itapipoca',
         state: 'Ceará',
-        street: 'Av Duque de Caxias',
+        street: 'Av. Duque de Caxias',
+        neighborhood: 'Centro',
         number: '553'
       }
     },
@@ -67,6 +69,7 @@ Organization.create(
         city: 'Itapipoca',
         state: 'Ceará',
         street: 'Rua Chico Viriato',
+        neighborhood: 'Área Nobre',
         number: '357'
       }
     }
@@ -74,29 +77,41 @@ Organization.create(
 )
 
 puts 'Criando contatos'
-
-Organization.all.each do |org|
-  Contact.create(
-    kind: rand(1..2),
-    value: Faker::PhoneNumber.phone_number,
-    organization: org
-  )
+3.times do
+  Organization.all.each do |org|
+    Contact.create(
+      kind: rand(1..2),
+      value: Faker::PhoneNumber.phone_number,
+      organization: org
+    )
+  end
 end
 
+puts 'criando usuário padrão'
 OrganizationUser.create(
   email: 'u@u',
   organization_id: 1,
   password: 'qwe123'
 )
 
+puts 'criando especialidades'
 Expertise.create(
   [
     [
-      name: 'Otorrino',
+      name: 'Cardiologia',
+      description: Faker::Lorem.sentence
+
+    ],
+    [
+      name: 'Dermatologia',
       description: Faker::Lorem.sentence
     ],
     [
-      name: 'Pediatra',
+      name: 'Ginecologia',
+      description: Faker::Lorem.sentence
+    ],
+    [
+      name: 'Otorrino',
       description: Faker::Lorem.sentence
     ],
     [
@@ -104,27 +119,27 @@ Expertise.create(
       description: Faker::Lorem.sentence
     ],
     [
-      name: 'Psiquiatria',
-      description: Faker::Lorem.sentence
-    ],
-    [
-      name: 'Cardiologia',
-      description: Faker::Lorem.sentence
-
-    ],
-    [
       name: 'Obstetricia',
       description: Faker::Lorem.sentence
     ],
     [
-      name: 'Ginecologia',
+      name: 'Pediatra',
+      description: Faker::Lorem.sentence
+    ],
+    [
+      name: 'Psiquiatria',
+      description: Faker::Lorem.sentence
+    ],
+    [
+      name: 'Radiologia',
       description: Faker::Lorem.sentence
     ]
   ]
 )
 
+puts 'criando médicos'
 20.times do
-  org_id = rand(1..4)
+  org_id = rand(1..3)
   shift = if Organization.find(org_id).has_emergency?
             [true, false].sample
           else
@@ -137,13 +152,7 @@ Expertise.create(
   )
 end
 
-30.times do
-  MedicExpertise.create(
-    expertise_id: rand(1..7),
-    medic_id: rand(1..20)
-  )
-end
-
+puts 'criando provedores de planos de saúde'
 HealthInsurer.create(
   [
     [
@@ -161,6 +170,7 @@ HealthInsurer.create(
   ]
 )
 
+puts 'criando planos de saúde'
 HealthPlan.create(
   [
     [
@@ -186,33 +196,64 @@ HealthPlan.create(
   ]
 )
 
+# variáveis para facilitar rand() caso alguma coisa seja adicionada
+health_plan_amount = HealthPlan.count
+organization_amount = Organization.count
+expertise_amount = Expertise.count
+
+puts 'criando relação entre planos e organizações'
 10.times do
   OrganizationHealthPlanExpertise.create(
-    organization_id: rand(1..3),
-    health_plan_id: rand(1..4),
-    expertise_id: rand(1..7)
+    organization_id: rand(1..organization_amount),
+    health_plan_id: rand(1..health_plan_amount),
+    expertise_id: rand(1..expertise_amount)
   )
 end
 
-Organization.all.each do |org|
-  OrganizationSetting.create(
-    interval_id: rand(1..5),
-    organization_id: org.id
+puts 'criando especialidades médicas'
+30.times do
+  MedicExpertise.create(
+    expertise_id: rand(1..expertise_amount),
+    medic_id: rand(1..20)
   )
 end
 
+puts 'criando intervalos'
+4.times do
+  Interval.create(
+    description: 'intervalo de pessoas presentes',
+    start: rand(1..5),
+    finish: rand(40..200),
+    step: rand(10..20),
+    counts: rand(2..5)
+  )
+end
+
+puts 'criando settings'
+4.times do
+  Organization.all.each do |org|
+    OrganizationSetting.create(
+      interval_id: rand(1..5),
+      organization_id: org.id
+    )
+  end
+end
+
+puts 'criando occupancy'
 10.times do |_i|
   Occupancy.create(
     start: rand(1..5),
     finish: rand(5..20),
-    organization_id: rand(1..3)
+    organization_id: rand(1..organization_amount)
   )
 end
+
+puts 'criando denúncias'
 
 10.times do
   Report.create(
     name: 'Denúncia',
     description: Faker::Lorem.sentence,
-    organization_id: rand(1..3)
+    organization_id: rand(1..organization_amount)
   )
 end
