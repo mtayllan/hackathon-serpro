e555d66e4acf694b3249c3011179eeb51301094b
Messias Tayllan <messiastayllan@gmail.com>
2020-06-20 21:59:30 -0300

    ✨ Get user location

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,19 @@
 //= require application
 //= require shards
+
+window.onload = () => {
+  const onSuccess = (position) => {
+    document.getElementsByName('latitude').value = position.coords.latitude;
+    document.getElementsByName('longitude').value = position.coords.longitude;
+    console.log('Posição encontrada :)');
+  }
+
+  const onError = (error) => {
+    console.log(error);
+    iziToast.error({
+      'message': 'Ops, não conseguimos encontrar sua localização para obter os resultados mais próximos'
+    });
+
+  }
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
