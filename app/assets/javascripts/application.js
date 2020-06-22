05a308ea895f4f092871c7040715e575101b5f1d
Messias Tayllan <messiastayllan@gmail.com>
2020-06-21 21:38:19 -0300

    remove turbolinks from app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require rails-ujs
-//= require turbolinks
 //= require bootstrap/dist/js/bootstrap.bundle.min
 //= require izitoast/dist/js/iziToast.min
 //= require bootstrap-select/dist/js/bootstrap-select.min
--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,3 +1,4 @@
 //= require application
+//= require turbolinks
 //= require sbadmin2
 //= require cocoon
